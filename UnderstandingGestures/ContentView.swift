47a0c243ce59ee6a532662c2f4f6be3adcc7b145
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-02-25 22:52:30 +0200

    Using the Gesture Modifier

--- UnderstandingGestures/ContentView.swift
+++ UnderstandingGestures/ContentView.swift
@@ -9,17 +9,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPressed = false
+    
     var body: some View {
         Image(systemName: "star.circle.fill")
             .font(.system(size: 200))
+            .scaleEffect(isPressed ? 0.5 : 1.0)
+            .animation(.easeInOut)
             .foregroundColor(.green)
-            .gesture(
-                TapGesture()
-                    .onEnded({
-                        print("Tapped!")
-                        
-                    }
-                )
+            .gesture( TapGesture()
+            .onEnded({
+                self.isPressed.toggle()
+            })
         )
     }
 }
